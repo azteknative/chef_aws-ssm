421696f0aa6288da609ba600d42275f11904df23
Peter Green <peter.green@aztek-native.com>
2017-12-12 14:45:43 +0000

    Add get_parameters_by_path support.

--- libraries/aws_ssm_paramter_store.rb
+++ libraries/aws_ssm_paramter_store.rb
@@ -20,5 +20,27 @@ class Chef::Recipe::AwsSsmParameterStore
     end
     result.parameter.value
   end
+
+  def self.get_parameters_by_path(path, recursive=false, with_decryption=false, region, aws_access_key_id, aws_secret_access_key)
+    if aws_access_key_id.nil? && aws_secret_access_key.nil?
+      client = Aws::SSM::Client.new(region: region)
+    else
+      client = Aws::SSM::Client.new(credentials: Aws::Credentials.new(aws_access_key_id, aws_secret_access_key), region: region)
+    end
+
+    begin
+      result = client.get_parameters_by_path({ path: path, recursive: recursive, with_decryption: with_decryption})
+    rescue Aws::SSM::Errors::ParameterNotFound
+      Chef::Log.debug("Parameter with name #{name} not found.")
+      return nil
+    rescue Aws::SSM::Errors::ServiceError => e
+      Chef::Log.fatal('Error querying SSM API!')
+      Chef::Log.debug("Exception is: #{e}")
+      raise e
+    end
+    parameters = {}
+    result.parameters.each { |p|  parameters[p.name] = p.value }
+    parameters
+  end
 end
 
--- test/fixtures/cookbooks/aws-ssm-test/recipes/default.rb
+++ test/fixtures/cookbooks/aws-ssm-test/recipes/default.rb
@@ -3,3 +3,5 @@ include_recipe 'aws-ssm'
 
 puts "Property found: #{AwsSsmParameterStore.get_parameter('test-data', true, 'us-east-1', node['aws-ssm-test']['aws_access_key_id'], node['aws-ssm-test']['aws_secret_access_key'])}"
 puts "Property not found: #{AwsSsmParameterStore.get_parameter('no-test-data', true, 'us-east-1', node['aws-ssm-test']['aws_access_key_id'], node['aws-ssm-test']['aws_secret_access_key'])}"
+
+puts "Properties found: #{AwsSsmParameterStore.get_parameters_by_path('/test-path/testing', true, true, 'us-east-1', node['aws-ssm-test']['aws_access_key_id'], node['aws-ssm-test']['aws_secret_access_key'])}"
